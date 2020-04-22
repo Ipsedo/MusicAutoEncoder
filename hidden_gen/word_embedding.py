63fdff241f3b21e1aebb3698045ac9bb203439a5
Ipsedo <berriens@gmail.com>
2020-04-22 14:22:29 +0200

    word embedding ok

--- hidden_gen/word_embedding.py
+++ hidden_gen/word_embedding.py
@@ -8,9 +8,21 @@ import re
 import torch as th
 import gensim
 
+from tqdm import tqdm
 
-def parse_wiki_dump(wiki_xml_file: str) -> List[str]:
-    pass
+
+def parse_wiki_dump(wiki_xml_file: str, limit=2000000) -> List[str]:
+    xml_f = open(wiki_xml_file, "r")
+
+    sentences = []
+    for i, l in tqdm(enumerate(xml_f)):
+        if i > limit:
+            break
+        sentences.append(l)
+
+    res = [re.sub(r"</?[a-z]+>", "", s) for s in tqdm(sentences)]
+
+    return res
 
 
 def gen_embedding(ft_model_file: str, sentence: str, hidden_length: int, n_channel: int) -> th.Tensor:
@@ -21,7 +33,8 @@ def gen_embedding(ft_model_file: str, sentence: str, hidden_length: int, n_chann
     ft = gensim.models.fasttext.FastText.load(ft_model_file)
 
     for i, w in enumerate(words):
-        res[0, i, :] = th.tensor(ft.wv[w], dtype=th.float)
+        vec = th.tensor(ft.wv[w], dtype=th.float)
+        res[0, i, :] = vec
 
     return res.permute(0, 2, 1)
 
@@ -42,7 +55,11 @@ def main() -> None:
     assert exists(input_file), f"Input text file \"{input_file}\" doesn't exist."
     assert isfile(input_file), f"\"{input_file}\" is not a file."
 
-    ft = gensim.models.fasttext.FastText(corpus_file=input_file, size=size, workers=8)
+    sentences = parse_wiki_dump(input_file)
+
+    print(len(sentences))
+
+    ft = gensim.models.fasttext.FastText(sentences=sentences, size=size, workers=8, window=5, word_ngrams=5, cbow_mean=0)
 
     ft.save(out_ft_file)
 
