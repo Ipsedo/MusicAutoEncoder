cb78278f51cab3029d99c60bb197eb8a6101b02a
Ipsedo <berriens@gmail.com>
2020-04-25 14:07:42 +0200

    train gan -> need fine tune

--- train_gan.py
+++ train_gan.py
@@ -6,14 +6,15 @@ from os.path import join, exists, isdir
 from tqdm import tqdm
 
 import torch as th
+import torch.nn as nn
 
 import random
 
-import auto_encoder
+import networks
 
 
 def main() -> None:
-    coder_maker = auto_encoder.CoderMaker()
+    coder_maker = networks.CoderMaker()
 
     parser = argparse.ArgumentParser("Train audio auto-encoder")
 
@@ -54,84 +55,85 @@ def main() -> None:
 
     print(data.size())
 
-    dec = coder_maker["decoder", archi, n_fft]
-    disc = auto_encoder.Discriminator(n_fft * 2)
+    gen = coder_maker["decoder", archi, n_fft]
+    disc = networks.DiscriminatorCNN(n_fft)
 
-    dec = dec.to(th.device("cuda"))
+    gen = gen.to(th.device("cuda"))
     disc = disc.to(th.device("cuda"))
 
-    optim_gen = th.optim.Adam(dec.parameters(), lr=lr_gen)
+    optim_gen = th.optim.Adam(gen.parameters(), lr=lr_gen)
     optim_disc = th.optim.Adam(disc.parameters(), lr=lr_discriminator)
 
-    hidden_size = seconds * sample_rate // n_fft // dec.division_factor()
+    hidden_size = seconds * sample_rate // n_fft // gen.division_factor()
 
-    assert data.size(2) // dec.division_factor() == hidden_size, \
-        f"Wrong hidden size, data : {data.size(2) // dec.division_factor()}, expected : {hidden_size}"
+    assert data.size(2) // gen.division_factor() == hidden_size, \
+        f"Wrong hidden size, data : {data.size(2) // gen.division_factor()}, expected : {hidden_size}"
 
-    nb_epoch = 15
-    batch_size = 1
+    nb_epoch = 30
+    batch_size = 4
     nb_batch = data.size(0) // batch_size
 
+    print("Train discriminator")
+
+    loss_fn = nn.BCELoss().cuda()
+
+    # Train Disc
     for e in range(nb_epoch):
 
+        # Discriminator
         sum_loss_disc = 0
         nb_backward_disc = 0
 
         sum_loss_gen = 0
-        nb_backward_gen = 0
+        nb_backward_gen = 1
 
         tqdm_bar = tqdm(range(nb_batch))
         for b_idx in tqdm_bar:
-
             i_min = b_idx * batch_size
             i_max = (b_idx + 1) * batch_size
             i_max = i_max if i_max < data.size(0) else data.size(0)
 
-            x_real = data[i_min:i_max, :, :].permute(0, 2, 1).flatten(0, 1)
+            x_real = data[i_min:i_max, :, :].cuda()
+            z_fake = th.randn(x_real.size(0), gen.get_hidden_size(), hidden_size, dtype=th.float).cuda()
 
-            z_fake = th.randn(x_real.size(0), dec.get_hidden_size(), hidden_size, dtype=th.float).cuda()
-            x_fake = dec(z_fake).permute(0, 2, 1).flatten(0, 1)
+            # Discriminator
+            disc.train()
+            gen.eval()
 
-            batch_size_disc = data.size(2) // 12 # 3
-            nb_batch_disc = x_real.size(0) // batch_size_disc
+            x_fake = gen(z_fake)
 
-            for b_idx_disc in range(nb_batch_disc):
-                i_min_disc = b_idx_disc * batch_size_disc
-                i_max_disc = (b_idx_disc + 1) * batch_size_disc
-                i_max_disc = i_max_disc if i_max_disc < x_real.size(0) else x_real.size(0)
+            out_real = disc(x_real)
+            out_fake = disc(x_fake)
 
-                if i_max_disc - i_min_disc < 2:
-                    continue
+            optim_disc.zero_grad()
+            loss = networks.discriminator_loss(out_real, out_fake)
+            loss.backward()
+            optim_disc.step()
 
-                x_r_b = x_real[i_min_disc:i_max_disc].cuda()
-                x_f_b = x_fake[i_min_disc:i_max_disc]
+            sum_loss_disc += loss.item()
+            nb_backward_disc += 1
 
-                out_real = disc(x_r_b)
-                out_fake = disc(x_f_b)
+            # Generator
+            disc.eval()
+            gen.train()
 
-                # Discriminator
-                loss = auto_encoder.discriminator_loss(out_real, out_fake)
-                optim_disc.zero_grad()
-                loss.backward(retain_graph=True)
-                optim_disc.step()
+            z_fake = th.randn(i_max - i_min, gen.get_hidden_size(), hidden_size, dtype=th.float).cuda()
+            x_fake = gen(z_fake)
+            out_fake = disc(x_fake)
 
-                sum_loss_disc += loss.item()
-                nb_backward_disc += 1
+            optim_gen.zero_grad()
+            loss = networks.generator_loss_2(out_fake)
+            loss.backward()
+            optim_gen.step()
 
-                # Generator
-                loss = auto_encoder.generator_loss(out_fake)
-                optim_gen.zero_grad()
-                loss.backward(retain_graph=True)
-                optim_gen.step()
+            sum_loss_gen += loss.item()
+            nb_backward_gen += 1
 
-                sum_loss_gen += loss.item()
-                nb_backward_gen += 1
+            tqdm_bar.set_description(f"Epoch {e:2d} : "
+                                     f"disc_avg = {sum_loss_disc / nb_backward_disc:.6f}, "
+                                     f"gen_avg = {sum_loss_gen / nb_backward_gen:.6f} ")
 
-                tqdm_bar.set_description(f"Epoch {e:2d} : "
-                                         f"disc_avg = {sum_loss_disc / nb_backward_disc:.6f}, "
-                                         f"gen_avg = {sum_loss_gen / nb_backward_gen:.6f}")
-
-        th.save(dec.state_dict(), join(out_dir, f"{dec}_epoch-{e}.th"))
+        th.save(gen.state_dict(), join(out_dir, f"{gen}_epoch-{e}.th"))
         th.save(optim_gen.state_dict(), join(out_dir, f"optim_gen_epoch-{e}.th"))
         th.save(disc.state_dict(), join(out_dir, f"{disc}_epoch-{e}.th"))
         th.save(optim_disc.state_dict(), join(out_dir, f"optim_disc_epoch-{e}.th"))
